567a67b0cc81e3593b38841bf3403a0208e124cd
kangyeon9525 <kangyeon9525@gmail.com>
2024-11-20 17:41:42 +0900

    fix - cors 추가 (모든 도메인 허용)

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import errorHandler from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config(); // .env 파일 로드
 
+
+const corsOptions = {
+  origin: '*', // 모든 도메인 허용
+  credentials: true, // 쿠키를 포함한 요청을 허용 
+};
+
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/users', userRoutes);
