3f1e758e4a993bb4c8b13559287e23f6162d1ab6
kangyeon9525 <kangyeon9525@gmail.com>
2024-11-24 11:08:56 +0900

    fix - http로 변경

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
-import http from 'http';
 import { Server } from 'socket.io';
 import app from './app.js';
+import http from 'http';
 import https from 'https';
 import fs from 'fs';
 
@@ -9,15 +9,16 @@ dotenv.config({ override: true }); // .env 파일 로드
 
 
 // SSL 인증서 로드
-const SSL_KEY_PATH = '/etc/letsencrypt/live/www.hwouu.shop/privkey.pem';
-const SSL_CERT_PATH = '/etc/letsencrypt/live/www.hwouu.shop/fullchain.pem';
-const sslOptions = {
-  key: fs.readFileSync(SSL_KEY_PATH), // SSL 키 파일
-  cert: fs.readFileSync(SSL_CERT_PATH), // SSL 인증서 파일
-};
+//const SSL_KEY_PATH = '/etc/letsencrypt/live/www.hwouu.shop/privkey.pem';
+//const SSL_CERT_PATH = '/etc/letsencrypt/live/www.hwouu.shop/fullchain.pem';
+//const sslOptions = {
+//  key: fs.readFileSync(SSL_KEY_PATH), // SSL 키 파일
+//  cert: fs.readFileSync(SSL_CERT_PATH), // SSL 인증서 파일
+//};
 
 // HTTP 및 Socket.io 서버 생성
-const server = https.createServer(sslOptions, app);
+//const server = https.createServer(sslOptions, app);
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*', // 모든 도메인 허용 (필요에 따라 제한 가능)
@@ -41,4 +42,4 @@ io.on('connection', (socket) => {
 });
 
 // 서버 시작
-server.listen(443, () => console.log('Server started on port 3000'));
+server.listen(3000, () => console.log('Server started on port 3000'));
